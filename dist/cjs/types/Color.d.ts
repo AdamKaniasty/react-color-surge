aad7b576968165b6511ba04ed370050ed09f2ba5
AdamKaniasty <adam.kaniasty@gmail.com>
2023-04-23 03:25:11 +0200

    0.1.11

--- dist/cjs/types/Color.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-export declare const Color: ({ hook, ...rest }: {
-    [x: string]: any;
-    hook: any;
-}) => JSX.Element;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare const Color: ({ hook, ...rest }: {
-    [x: string]: any;
-    hook: any;
-}) => JSX.Element;
-
-export { Color };
